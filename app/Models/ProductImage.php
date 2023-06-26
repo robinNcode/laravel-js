898504733bd89ea33b31c29695cf2a6a59102bc2
robinNcode <msmrobin518@gmail.com>
2023-06-26 12:52:10 +0600

    update: product variant, variant prices relation added

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductImage extends Model
 {
-    //
+    protected $fillable = [
+        'product_id', 'file_path', 'thumbnail'
+    ];
 }
--- app/Models/ProductVariantPrice.php
+++ app/Models/ProductVariantPrice.php
@@ -1,10 +1,31 @@
-<?php
-
-namespace App\Models;
+<?php namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class ProductVariantPrice extends Model
 {
+    protected $fillable = [
+        'product_variant_one',
+        'product_variant_two',
+        'product_variant_three',
+        'price',
+        'stock',
+        'product_id'
+    ];
 
+    public function variantOne(): HasOne
+    {
+        return $this->hasOne(ProductVariant::class, 'id', 'product_variant_one');
+    }
+
+    public function variantTwo(): HasOne
+    {
+        return $this->hasOne(ProductVariant::class, 'id', 'product_variant_two');
+    }
+
+    public function variantThree(): HasOne
+    {
+        return $this->hasOne(ProductVariant::class, 'id', 'product_variant_three');
+    }
 }
